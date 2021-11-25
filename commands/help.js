4eb53f348d57703d11d4cbfed6d2eef33ce417d6
NM10100 <84334072+NM10100@users.noreply.github.com>
2021-11-25 15:21:41 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -123,7 +123,7 @@ module.exports = {
   Discord Music Bot Version: v${require("../package.json").version}
   [✨ Support Server](${
     client.botconfig.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | [Dashboard](${client.botconfig.Website}) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+  }) **By _NM10#2686**`);
       if (!args) return interaction.send(Embed);
       else {
         let cmd =
